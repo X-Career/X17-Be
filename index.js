a5c2bb105a15e59acee410bad55556c015884327
Minh Quân <mquanv12@gmail.com>
2023-09-20 01:41:25 +0700

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const { PORT } = process.env || 9000;
+const { MONGO_URL } = process.env;
+
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.get("/", (_, res) => {
+      res.send({
+        message: "success",
+      });
+    });
+
+    app.listen(PORT, () =>
+      console.log(`Server running on: http://localhost:${PORT}`)
+    );
+  })
+  .catch((error) => console.log(error));
